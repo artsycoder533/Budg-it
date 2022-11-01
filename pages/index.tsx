976d5d84b8f77286efba8316aa74a812e3331f1d
Natasha <njohnson533@gmail.com>
2022-10-31 21:45:35 -0400

    dep: add firebase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,12 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
     <div>
       <Head>
-        <title>ExpensesMD</title>
+        <title>Budg.it</title>
         <meta name="description" content="An app that auto-generates a budget for the users to help them reach financial independence." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
