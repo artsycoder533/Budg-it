672b15ca0ae46c471bcad430a28c27f762d7f00a
Natasha <njohnson533@gmail.com>
2022-11-30 23:52:49 -0500

    chore: remove return from map function

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,23 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import { SlArrowRight } from "react-icons/sl";
 
-const formSteps: string[] = ["Personal Details", "Income", "Debt", "Fixed Expenses", "Variable Expenses", "Emergency Fund", "Goals" ]
+const formSteps: string[] = [
+  "Personal Details",
+  "Income",
+  "Debt",
+  "Fixed Expenses",
+  "Variable Expenses",
+  "Emergency Fund",
+  "Goals",
+];
 
 const Sidebar = () => {
-  const [selected, setSelected] = useState<boolean>(false)
+  const [selected, setSelected] = useState<boolean>(false);
   //keep track of steps to know which form to show and which link should be highlighted
   return (
     <div className="w-56 border h-96">
-      {formSteps.map((el, index) => {
-        return (
-          <button
-            className="border p-3 pointer flex items-center gap-2 w-full"
-            onClick={() => setSelected(!selected)} key={index}>
-            {selected && <SlArrowRight className="text-blue-700" />}
-            {el}
-          </button>
-        );
-      })}
+      {formSteps.map((el, index) => (
+        <button key={index}
+          className="border p-3 pointer flex items-center gap-2 w-full" onClick=
+          {() => setSelected(!selected)}>
+          {selected && <SlArrowRight className="text-blue-700" />}
+          {el}
+        </button>
+      ))}
     </div>
   );
 };
