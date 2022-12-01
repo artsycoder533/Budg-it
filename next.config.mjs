e1a0447c6bbd3bbc21f9c6eaa6d646f86bf8dafe
Natasha <njohnson533@gmail.com>
2022-12-01 00:04:19 -0500

    chore: update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     // loader: "akamai",
-    // path: "",
+    path: "",
     domains: ["lh3.googleusercontent.com"],
   },
   assetPrefix: "/",
