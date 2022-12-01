a4b00801d13a08feca42313807595c96f50a97e5
Natasha <njohnson533@gmail.com>
2022-11-30 22:28:42 -0500

    chore: add links to nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,9 +8,17 @@ function Navbar(): JSX.Element {
   const [user, loading] = useAuthState(auth);
 
   return (
-    <header className=" p-3 border">
-      <nav className="flex justify-between items-center max-w-[1200px] mx-auto">
+    <header className="">
+      <div className="text-center p-2 bg-blue-700 text-white font-bold">
         <Link href="/">Budg.it</Link>
+      </div>
+      <nav className="flex max-w-[1200px] mx-auto justify-between p-1">
+        <img
+          src={user?.photoURL as string}
+          alt="avatar"
+          className="object-cover w-12 h-12"
+          referrerPolicy="no-referrer"
+        />
         {!user ? (
           <Link href={"/auth/login"} legacyBehavior>
             <a className="py-2 px-4 text-md bg-teal-500 text-white rounded-md">
@@ -18,14 +26,18 @@ function Navbar(): JSX.Element {
             </a>
           </Link>
         ) : (
-          <div className="relative flex gap-2">
-            <img
-              src={user?.photoURL as string}
-              alt="avatar"
-              className="object-cover w-12 h-12"
-                referrerPolicy="no-referrer"
-              />
-              <button onClick={()=> auth.signOut()} className="flex items-center  gap-2 rounded-md py-2 px-4 bg-red-700 text-white"><FiLogOut /> Logout</button>
+          <div className="w-100 flex gap-4 items-center justify-end">
+            <Link href="/" className="">
+              Home
+            </Link>
+            <Link href="/assessment" className="">
+              Assessment
+            </Link>
+            <button
+              onClick={() => auth.signOut()}
+              className="flex items-center  gap-2 rounded-md py-2 px-4  text-blue-700 font-bold border-2 border-blue-700">
+              <FiLogOut className="font-bold text-blue-700" /> Logout
+            </button>
           </div>
         )}
       </nav>
